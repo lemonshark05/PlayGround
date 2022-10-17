6cbf52f77d4e27e18da585f626ed9c702f6d0d8e
lemonshark <zmeng8@usfca.com>
2022-10-17 00:46:16 -0700

    Initial Commit

---
+++ Tic-TacGame/Tic_TacGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_TacGameApp.swift
+//  Tic-TacGame
+//
+//  Created by lemonshark on 2022/10/17.
+//
+
+import SwiftUI
+
+@main
+struct Tic_TacGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
